6ad60ea715d08b84246495278382ad04a3842d33
jose221 <39977171+jose221@users.noreply.github.com>
2023-01-20 09:07:54 -0600

    fix 1 09/01/2023

--- routes/api-portfolio-categories.js
+++ routes/api-portfolio-categories.js
@@ -5,6 +5,7 @@ const AuthService = require('../app/services/AuthService');
 let path = "/portfolio-categories";
 
 router.get(`${path}`, async (req, res) => Controller.get(req, res, await AuthService.getTokenDecrypt(req.header('auth-token'))));
+router.get(`${path}/:id`, async (req, res) => Controller.get(req, res, await AuthService.getTokenDecrypt(req.header('auth-token'))));
 router.post(`${path}`, async (req, res) => Controller.create(req, res, await AuthService.getTokenDecrypt(req.header('auth-token'))));
 router.put(`${path}/:id`, async (req, res) => Controller.update(req, res, await AuthService.getTokenDecrypt(req.header('auth-token'))));
 router.delete(`${path}/:id`, async (req, res) => Controller.delete(req, res, await AuthService.getTokenDecrypt(req.header('auth-token'))));
