a088f885ccbdbab0af86d8097d1ef22b514827a5
jose221 <39977171+jose221@users.noreply.github.com>
2023-02-14 20:44:00 -0600

    ip publica client

--- app/controllers/HerandroDataController.js
+++ app/controllers/HerandroDataController.js
@@ -1,13 +1,13 @@
 const {v4: uuidv4} = require("uuid");
 class HerandroDataController {
     init(socket, req={}){
-        var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
+        var ip = req.connection.remoteAddress || "anonimo";
         req.id = uuidv4()
         socket.send(JSON.stringify({...req, ip}));
     }
 
     action(socket, req ={}){
-        var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
+        var ip = req.connection.remoteAddress || "anonimo";
         socket.send(JSON.stringify({...req, ip}));
     }
 }
