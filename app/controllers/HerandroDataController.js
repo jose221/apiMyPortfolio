eee9d16ef79a53daafbf9b05e18343e5edece65d
jose221 <39977171+jose221@users.noreply.github.com>
2023-02-14 20:46:04 -0600

    correcciones

--- app/controllers/HerandroDataController.js
+++ app/controllers/HerandroDataController.js
@@ -3,12 +3,12 @@ class HerandroDataController {
     init(socket, req={}){
         var ip = req.connection.remoteAddress || "anonimo";
         req.id = uuidv4()
-        socket.send(JSON.stringify({...req, ip}));
+        socket.send(JSON.stringify({...req, ip:ip}));
     }
 
     action(socket, req ={}){
         var ip = req.connection.remoteAddress || "anonimo";
-        socket.send(JSON.stringify({...req, ip}));
+        socket.send(JSON.stringify({...req, ip:ip}));
     }
 }
 module.exports = new HerandroDataController();
